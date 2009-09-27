e7fa6291cbab28cceb53e8a04f64a3ee775aff1f
Jeremy Johnstone <jstone@air-33j-net.local>
2009-09-26 23:38:35 -0700

    further performance tweaks, gettind down to metal now

--- Sim2Virus/Parser/Reader.cpp
+++ Sim2Virus/Parser/Reader.cpp
@@ -3,44 +3,54 @@ using namespace std;
 
 MATRIX read_input_data(const char *filename) {
 	MATRIX dataset;
-	char current = 0;
-	char previous = 0;
-	ifstream inputstream;
+	int current = 0;
+	int previous = 0;
 
-	inputstream.open(filename);
-
-	ROW row = ROW();
-	TABLE table = TABLE();
-	while(inputstream.good()) {
-		inputstream.get(current);
-		switch (current) {
-			case 32:
-				continue;
-		    case 10:
-		        if(previous == 10) {
-					if(table.size() > 0) {
-						dataset.push_back(table);
-			        	table = TABLE();
-					}
-					break;
-		        }
-				if(row.size() > 0) {
-					table.push_back(row);
-			        row = ROW();
-				}
-		        break;
-			case 48:
-		        row.push_back(0);
-		        break;
-		    case 49:
-		        row.push_back(1);
-		        break;
-		    case 50:
-		        row.push_back(2);
-		        break;
-		}
-		previous = current;
+    FILE* input = fopen(filename, "r");
+	if(!input) {
+		cerr << "couldn't read from: " << filename << endl;
+		exit(2);
 	}
+
+    ROW row = ROW();
+    TABLE table = TABLE();
+
+    current = fgetc(input);
+
+    while( current != EOF ) {
+        switch (current) {
+            case 32:
+                current = fgetc(input);
+				continue;
+            case 10:
+                if(previous == 10 && 0 != table.size()) {
+                	dataset.push_back(table);
+	                table = TABLE();
+					goto loop_end2;
+	            }
+				if(0 != row.size()) {
+					table.push_back(row);
+	                row = ROW();
+				}
+				goto loop_end;
+            case 48:
+                row.push_back(0);
+                goto loop_end;
+            case 49:
+                row.push_back(1);
+                goto loop_end;
+            case 50:
+                row.push_back(2);
+                goto loop_end;
+        }
+
+loop_end:		
+        previous = current;
+loop_end2:
+        current = fgetc(input);
+    }
+
+    fclose(input);
 	
 	return dataset;
 }
