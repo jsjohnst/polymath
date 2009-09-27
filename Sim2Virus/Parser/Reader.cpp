96e1573a8d0307ae821e66a1c6a96af4be959b4e
Jeremy Johnstone <jstone@air-33j-net.local>
2009-09-26 22:47:31 -0700

    even more smoking fast via optimizations in reading input

--- Sim2Virus/Parser/Reader.cpp
+++ Sim2Virus/Parser/Reader.cpp
@@ -12,24 +12,33 @@ MATRIX read_input_data(const char *filename) {
 	ROW row = ROW();
 	TABLE table = TABLE(400);
 	while(inputstream.good()) {
-		current = inputstream.get();
-		if(current == 32) {
-			continue;
-		}
-		if(current == 10) {
-			if(previous == 10) {
-				dataset.push_back(table);
-				table = TABLE(400);
+		inputstream.get(current);
+		switch (current) {
+			case 32:
 				continue;
-			}
-			previous = current;
-			table.push_back(row);
-			row = ROW();
-			continue;
+		    case 10:
+		        if(previous == 10) {
+					if(table.size() > 0) {
+						dataset.push_back(table);
+			        	table = TABLE();
+					}
+		        	continue;
+		        }
+		        previous = current;
+		        table.push_back(row);
+		        row = ROW();
+		        continue;
+			case 48:
+		        row.push_back(0);
+		        break;
+		    case 49:
+		        row.push_back(1);
+		        break;
+		    case 50:
+		        row.push_back(2);
+		        break;
 		}
-
 		previous = current;
-		row.push_back(atoi(&current));
 	}
 	
 	return dataset;
